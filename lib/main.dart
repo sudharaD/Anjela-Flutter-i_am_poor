009da9737e0ebc0a3722e427e31b6fce4f23180c
sudharaD <sudharad7@gmail.com>
2023-01-01 11:51:08 +0530

    I am poor - 3

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ void main() {
           title: Text('I am Rich'),
           backgroundColor: Colors.amber[700],
         ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
+        ),
       ),
     ),
   );
